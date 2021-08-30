c56bda130f7c4bb4ce1ff545235950c72790d2d4
John Clyde <john.clyde@mozartdata.com>
2021-08-30 13:09:31 -0700

    Unpin the requirements.

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(name='tap-slack',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_slack'],
       install_requires=[
-          'singer-python==5.9.0',
-          'slackclient==2.6.0',
+          'singer-python>=5.9.0',
+          'slackclient>=2.6.0',
       ],
       extras_require={
           'dev': [
